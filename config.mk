51022ab3f4927b74d60ebe8d69aac93a3005a430
techyminati <techyminati@outlook.com>
2026-06-23 11:47:28 +0530

    config: Enable background blur for widget picker
    Change-Id: Ice370f166a73d395e223a8892a6f3350ce94106d
    Signed-off-by: techyminati <techyminati@outlook.com>

--- config.mk
+++ config.mk
@@ -55,6 +55,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
 
+# Enable background blur for widget picker
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.launcher.depth.widget=true
+
 # Gboard Props
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.theme_id=5 \
