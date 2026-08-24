9e7f6e8ca5ae566f01924782c3ca076bbfa3b22e
rwicept <thanhtrung08022010@gmail.com>
2026-08-24 10:48:01 +0700

    gms: Initial blobs from panther CP2A.260705.006
    Co-authored-by: AdarshGrewal <adarshgrewal@gmail.com>
    Co-authored-by: aswin7469 <aswinas@pixysos.com>
    Co-authored-by: ralph950412 <ralph950412@gmail.com>

---
+++ common/overlays.mk
@@ -0,0 +1,26 @@
+PRODUCT_PACKAGES += \
+    AccessibilityMenuOverlayProduct \
+    BuiltInPrintServiceOverlayProduct \
+    ContactsProviderOverlayProduct \
+    FrameworkResOverlayProduct \
+    GlanceableHubConfigOverlay \
+    GlanceableHubSettingsConfigOverlay \
+    GlanceableHubSettingsConfigOverlay2022 \
+    GlanceableHubSysuiConfigOverlay \
+    GoogleConfigOverlay \
+    GoogleWebViewOverlay \
+    IdentityCheckSettingsOverlay \
+    ManagedProvisioningPixelOverlay \
+    PixelConfigOverlay2018 \
+    PixelConfigOverlay2019 \
+    PixelConfigOverlay2019Midyear \
+    PixelConfigOverlay2021 \
+    PixelConfigOverlayCommon \
+    PixelSetupWizardOverlayExpressive \
+    SettingsGoogleOverlayProduct \
+    SettingsProviderOverlayProduct \
+    SimAppDialogOverlayProduct \
+    SystemUIGXOverlay \
+    SystemUIGoogleOverlayProduct \
+    TeleServiceOverlayProduct \
+    VerifierResOverlay
---
+++ config.mk
@@ -0,0 +1,64 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Artifact path requirement allowlist
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/app/GoogleAppSearchAiSealConfig/GoogleAppSearchAiSealConfig.apk \
+    system/app/CaptivePortalLoginGoogle/CaptivePortalLoginGoogle.apk \
+    system/app/GoogleExtShared/GoogleExtShared.apk \
+    system/app/GooglePrintRecommendationService/GooglePrintRecommendationService.apk \
+    system/etc/default-permissions/default-permissions_googleprintrecommendationservice.xml \
+    system/etc/permissions/com.google.android.networkstack.xml \
+    system/etc/permissions/privapp-permissions-google.xml \
+    system/etc/sysconfig/preinstalled-packages-generic-system-google.xml \
+    system/priv-app/NetworkStackGoogle/NetworkStackGoogle.apk \
+    system/priv-app/DocumentsUIGoogle/DocumentsUIGoogle.apk \
+    system/priv-app/GooglePackageInstaller/GooglePackageInstaller.apk \
+    system/priv-app/TagGoogle/TagGoogle.apk
+
+# Use gestures by default
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.vendor.overlay.theme=com.android.internal.systemui.navbar.gestural;com.google.android.systemui.gxoverlay
+
+# Gboard Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.com.google.ime.theme_id=5 \
+    ro.com.google.ime.system_lm_dir=/product/usr/share/ime/google/d3_lms
+
+# GMS Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.opa.eligible_device=true
+
+# SetupWizard Props
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.default_locale_enhancement_enabled=true \
+    setupwizard.feature.enable_gil= \
+    setupwizard.feature.enable_wifi_tracker=true \
+    setupwizard.feature.lifecycle_refactoring=true \
+    setupwizard.feature.locale_agnostic_enabled=true \
+    setupwizard.feature.notification_refactoring=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true \
+    setupwizard.theme=glif_expressive
+
+$(call inherit-product, vendor/gms/common/common-vendor.mk)
